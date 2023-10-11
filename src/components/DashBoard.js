8bd287724b7a420e7f9c7bcb3201164e89e0e464
joelrajusomala <joelrajusomala@gmail.com>
2023-10-12 00:26:17 +0530

    all components in dashboard

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react"
 import SideBar from "./SideBar"
-import Home from "./Home"
+
 import "./dashboard.css"
+import Nav from "./Nav"
 
 const DashBoard = () => {
     const [toggle, setToggle] = useState(true)
@@ -20,7 +21,80 @@ const DashBoard = () => {
                 )}
                 {toggle && <div className="col-4 col-md-2"></div>}
                 <div className="col px-0">
-                    <Home Toggle={Toggle}></Home>
+                    <Nav Toggle={Toggle} />
+
+                    <div className="px-2" Toggle={Toggle}>
+                        <div className="container-fluid ">
+                            <div className="row g-3 py-4">
+                                <div className="col-md-3">
+                                    <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                        <div>
+                                            <h3 className="fs-2">230</h3>
+                                            <p className="fs-5">Prouducts</p>
+                                        </div>
+                                        <i className="bi bi-cart-plus p-3 fs-1"></i>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                        <div>
+                                            <h3 className="fs-2">230</h3>
+                                            <p className="fs-5">Total </p>
+                                        </div>
+                                        <i className="bi bi-cart-plus p-3 fs-1"></i>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                        <div>
+                                            <h3 className="fs-2">230</h3>
+                                            <p className="fs-5">Total</p>
+                                        </div>
+                                        <i className="bi bi-cart-plus p-3 fs-1"></i>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                        <div>
+                                            <h3 className="fs-2">230</h3>
+                                            <p className="fs-5">Total</p>
+                                        </div>
+                                        <i className="bi bi-cart-plus p-3 fs-1"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <table class="table bg-white rounded mt-2">
+                            {/* <caption className="text-white fs-4">List of Jobs</caption> */}
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td colspan="2">Larry the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
